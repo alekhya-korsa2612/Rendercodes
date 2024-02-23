e4fc928430ad33652519330db3b30ab471927de0
alekhya-korsa2612 <150769034+alekhya-korsa2612@users.noreply.github.com>
2024-02-23 16:04:22 +0530

    Add files via upload

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,15 +1,13 @@
 const jwt = require('jsonwebtoken');
 
-
-module.exports = (req,res,next) =>{
-   
-    try{
-    const token = req.headers.authorization.split(' ')[1];
-    const decode = jwt.verify(token,"webBatch");
-    req.userData = decode;
-    // return res.json(decode);
-    next();
-    }catch(error){
-        res.json({success:false,message:"Auth failed"})
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decode = jwt.verify(token, "webBatch");
+        req.userData = decode;
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(401).json({ success: false, message: "Auth failed: Invalid token" });
     }
-}
+};
